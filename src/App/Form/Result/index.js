7549f05e5199fa882599b0747db92b2e060cb98b
Maciej Urny <maciej.urny@o2.pl>
2023-04-13 23:55:30 +0200

    paragraph deleted

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -4,11 +4,9 @@ export const Result = ({ result }) => (
     <>
         {result !== undefined && (
             <div className="container">
-                <p>
-                    {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;/&nbsp;
-                    {result.sourceRate.toFixed(2)}&nbsp;=&nbsp;
-                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-                </p>
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;/&nbsp;
+                {result.sourceRate.toFixed(2)}&nbsp;=&nbsp;
+                {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
             </div>
         )}
     </>
