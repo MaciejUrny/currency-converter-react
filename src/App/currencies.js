9ee5b07fd8965db6fad0dbe90753154bf19f0e13
Maciej Urny <maciej.urny@o2.pl>
2023-04-13 23:48:27 +0200

    code formatted

--- src/App/currencies.js
+++ src/App/currencies.js
@@ -2,21 +2,21 @@ export const currencies = [
     {
         short: "EUR",
         name: "Euro [EUR]",
-        value: 4.6898,      
+        value: 4.6898,
     },
     {
         short: "USD",
         name: "Dolar amerykański [USD]",
-        value: 4.7975,      
+        value: 4.7975,
     },
     {
         short: "CHF",
         name: "Frank szwajcarski [CHF]",
-        value: 4.7523,      
+        value: 4.7523,
     },
     {
         short: "GBP",
         name: "Funt szterling [GBP]",
-        value: 5.3825,      
+        value: 5.3825,
     },
 ];
