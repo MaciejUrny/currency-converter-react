21c7ce45864d4a56005e121c9f57da8d8241662a
Maciej Urny <maciej.urny@o2.pl>
2023-04-13 21:30:02 +0200

    currencies array added

---
+++ src/App/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro [EUR]",
+        value: 4.6898,      
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański [USD]",
+        value: 4.7975,      
+    },
+    {
+        short: "CHF",
+        name: "Frank szwajcarski [CHF]",
+        value: 4.7523,      
+    },
+    {
+        short: "GBP",
+        name: "Funt szterling [GBP]",
+        value: 5.3825,      
+    },
+];
