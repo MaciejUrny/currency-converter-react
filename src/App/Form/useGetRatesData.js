8988b3ca44d8b9d7590153c1c3ea4ff42e88e447
Maciej Urny <maciej.urny@o2.pl>
2024-02-26 22:01:14 +0100

    import data from api

---
+++ src/App/Form/useGetRatesData.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const urlDataApi = "https://api.currencyapi.com/v3/latest?apikey=cur_live_wGrdHqnccqnyociyJnNg0kLKkYZNgaqtLPF4S2hL&currencies=EUR%2CUSD%2CCHF%2CGBP&base_currency=PLN";
+
+export const getRatesData = async () => {
+    const response = await axios.get(urlDataApi);
+    if (response.status !== 200) {
+        throw new Error();
+    }
+    return response.data;
+};
